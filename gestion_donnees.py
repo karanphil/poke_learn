2b005861b1648f92e94f0bb6b88173a05970c140
Gabrielle Grenier <gabrielle.g.grenier@usherbrooke.ca>
2019-11-14 16:46:09 -0500

    Creation de la classe BaseDonnees

---
+++ gestion_donnees.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+#####
+# Gabrielle Grenier 15 065 448
+# Philippe Karan 15 093 532
+###
+
+import numpy as np
+import pandas as pd
+
+
+class BaseDonnees:
+    def __init__(self):
+        """
+        Classe effectuant le traitement de la base de données ainsi que 
+        l'analyse des éléments utiles dans la base de données.
+        """
+        self.bd = bd
+
+        
