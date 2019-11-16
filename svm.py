4a33c95047d00bddfcd78fb964705c9e5614379e
karanphil <philippe.karan@usherbrooke.ca>
2019-11-16 15:24:22 -0500

    squelette de la classe SVM

---
+++ svm.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from classifieur import Classifieur
+
+class SVM(Classifieur):
+    def __init__(self):
+        raise NotImplementedError
+
+    def validation_croisee(self):
+        raise NotImplementedError
+    
