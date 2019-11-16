29f7124c6a9e22e3bec41f1e877c39a80aa4decd
karanphil <philippe.karan@usherbrooke.ca>
2019-11-16 17:25:03 -0500

    ajout d'une possibilité de validation croisée, à voir

--- adaboost.py
+++ adaboost.py
@@ -5,3 +5,6 @@ from sklearn.ensemble import AdaBoostClassifier
 class AdaBoost(Classifieur):
     def __init__(self, base_estimateur = None):
         self.modele = AdaBoostClassifier(base_estimator = base_estimateur)
+
+    def validation_croisee(self, x_entr, t_entr, est_ech_poids = False, *args):
+        raise NotImplementedError
