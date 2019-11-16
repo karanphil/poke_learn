974508cf2d5a9d9efadcd9d72c24dbfc87804a43
karanphil <philippe.karan@usherbrooke.ca>
2019-11-16 17:04:45 -0500

    ajout de la classe init

--- fad.py
+++ fad.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from classifieur import Classifieur
+from sklearn.ensemble import RandomForestClassifier
 
 class FAD(Classifieur):
-    def __init__(self):
-        raise NotImplementedError
+    def __init__(self, nb_arbres = 10, critere = 'gini', bootstrap = True):
+        self.modele = RandomForestClassifier(n_estimators = nb_arbres, 
+                        criterion = critere, bootstrap = bootstrap)
