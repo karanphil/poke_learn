66e9d86fcf6d88b0bafac13eb553c4e155c15f64
karanphil <philippe.karan@usherbrooke.ca>
2019-11-18 09:48:29 -0500

    squelette du main()

---
+++ poke_learn.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+def main():
+    # Gestion des données
+
+    # Entrainement ou validation croisée
+
+    # Prédiction et erreur
+
+    # Analyse des résultats
+
+if __name__ == "__main__":
+    main()
