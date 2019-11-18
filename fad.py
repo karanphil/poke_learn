42d403e3e784167d87b3c55d8a92dab12aa3542d
karanphil <philippe.karan@usherbrooke.ca>
2019-11-18 16:11:31 -0500

    ajout du paramètre max_depth pour éviter le sur-apprentissage

--- fad.py
+++ fad.py
@@ -3,6 +3,6 @@ from classifieur import Classifieur
 from sklearn.ensemble import RandomForestClassifier
 
 class FAD(Classifieur):
-    def __init__(self, nb_arbres = 10, critere = 'gini', bootstrap = True):
+    def __init__(self, nb_arbres = 10, critere = 'gini', prof_max = None, bootstrap = True):
         self.modele = RandomForestClassifier(n_estimators = nb_arbres, 
-                        criterion = critere, bootstrap = bootstrap)
+                        criterion = critere, max_depth = prof_max, bootstrap = bootstrap)
