38cdfdd4e2eeb05825776ebbee2af374530215aa
karanphil <philippe.karan@usherbrooke.ca>
2019-11-27 00:13:59 -0500

    ajout d'un parser et modifications en conséquence

--- poke_learn.py
+++ poke_learn.py
@@ -12,33 +12,62 @@ from gestion_donnees import BaseDonnees
 def _build_args_parser():
     p = argparse.ArgumentParser(
         description=__doc__, formatter_class=argparse.RawTextHelpFormatter)
-    p.add_argument('fichier',
-                    help='Nom du fichier de données.')
-    p.add_argument('vc',
-                    help='Choix de validation croisée ou pas.')
-    p.add_argument('est_ech_poids',
+    p.add_argument("fichier", type=str,
+                    help="Nom du fichier de données.")
+    p.add_argument("vc", type=int, choices=[0,1],
+                    help="Choix de validation croisée ou pas.")
+    p.add_argument("est_ech_poids", type=int, choices=[0,1],
                     help="Choix de poids d'échantillon ou pas.")
-    p.add_argument('choix_modele',
-                    help='Choix du modèle à utiliser.')
+    p.add_argument("choix_modele", type=str, 
+                    choices=["bayes_naif","perceptron",
+                    "perceptron_mc","svm","fad","adaboost"],
+                    help="Choix du modèle à utiliser.")
+    p.add_argument("--tol_perceptron", type=float, default=1e-3,
+                    help="Critère de tolérance (perceptron).")
+    p.add_argument("--max_iter_perceptron", type=int, default=1000,
+                    help="Maximum d'itérations (perceptron).")
+    p.add_argument("--couches_cachees", type=str, default=(2,5,2),
+                    help="Nombre de neurones par couches (perceptron_mc).")
+    p.add_argument("--activation", type=str, default="relu", 
+                    choices=["identity","logistic","tanh","relu"],
+                    help="Type de fonction d'activation (perceptron_mc).")
+    p.add_argument("--solutionneur", type=str, default="sgd", 
+                    choices=["lbfgs","sgd","adam"],
+                    help="Type de solutionneur (perceptron_mc).")
+    p.add_argument("--max_iter_perceptron_mc", type=int, default=2000,
+                    help="Maximum d'itérations (perceptron_mc).")
+    p.add_argument("--noyau", type=str, default="rbf",
+                    choices=["linear","poly","rbf","sigmoid"],
+                    help="Type de noyau (svm).")
+    p.add_argument("--tol_svm", type=float, default=1e-3,
+                    help="Critère de tolérance (svm).")
+    p.add_argument("--max_iter_svm", type=int, default=-1,
+                    help="Maximum d'itérations (svm).")
+    p.add_argument("--nb_arbres", type=int, default=10,
+                    help="Nombre d'arbres dans la forêt (fad).")
+    p.add_argument("--critere", type=str, default="gini",
+                    choices=["gini","entropy"],
+                    help="Critère de séparation (fad).")
+    p.add_argument("--prof_max_fad", type=int, default=None,
+                    help="Profondeur maximale d'un arbre (fad).")
+    p.add_argument("--prof_max_adaboost", type=int, default=1,
+                    help="Profondeur maximale de l'arbre (adaboost).")
 
     return p
 
 def main():
-    fichier = sys.argv[1]
-    vc = int(sys.argv[2])
-    est_ech_poids = int(sys.argv[3])
-    i = 4
-    modele_choix = sys.argv[i]
+    parser = _build_args_parser()
+    args = parser.parse_args()
 
     # Gestion des données
-    bd = BaseDonnees(fichier, 'is_legendary')
+    bd = BaseDonnees(args.fichier, 'is_legendary')
     liste_colonne = bd.voir_att()
     bd.enlever_attributs(['abilities', 'japanese_name', 'name', 'generation', 'pokedex_number', 'classfication'])
     bd.str_a_int(['capture_rate'])
     bd.str_a_vec(['type1', 'type2'])
     bd.normaliser_donnees()
     bd.methode_filtrage()
-    if(bool(est_ech_poids)):
+    if(bool(args.est_ech_poids)):
         poids = bd.definir_poids_att()
     else:
         poids = []
@@ -47,38 +76,28 @@ def main():
 
     # Gestion du modèle
     print("Création du mondèle...")
-    if(modele_choix == "bayes_naif"):
+    if(args.choix_modele == "bayes_naif"):
         modele = BayesNaif()
-    elif(modele_choix == "perceptron"):
-        modele = Perceptron(max_iter = 1000, tol = 1e-3)
-    elif(modele_choix == "perceptron_mc"):
-        couches_cachees = tuple([int(x) for x in sys.argv[i+1].split(',')])
-        activation = sys.argv[i+2]
-        solutionneur = sys.argv[i+3]
-        modele = PerceptronMC(couches_cachees = couches_cachees, 
-                    activation = activation, solutionneur = solutionneur, max_iter = 20000)
-    elif(modele_choix == "svm"):
-        noyau = sys.argv[i+1]
-        modele = SVM(noyau = noyau, tol = 1e-3, max_iter = -1)
-    elif(modele_choix == "fad"):
-        nb_arbres = int(sys.argv[i+1])
-        critere = sys.argv[i+2]
-        prof_max = int(sys.argv[i+3])
-        modele = FAD(nb_arbres = nb_arbres, critere = critere, prof_max = prof_max)
-    elif(modele_choix == "adaboost"):
-        max_prof = int(sys.argv[i+1])
-        modele = AdaBoost(max_prof = max_prof)
-    else:
-        print("Oups, ce modèle n'existe pas!")
-        return 0
+    elif(args.choix_modele == "perceptron"):
+        modele = Perceptron(max_iter = args.max_iter_perceptron, tol = args.tol_perceptron)
+    elif(args.choix_modele == "perceptron_mc"):
+        modele = PerceptronMC(couches_cachees = tuple([int(x) for x in args.couches_cachees.split(',')]), 
+                    activation = args.activation, solutionneur = args.solutionneur,
+                    max_iter = args.max_iter_perceptron_mc)
+    elif(args.choix_modele == "svm"):
+        modele = SVM(noyau = args.noyau, tol = args.tol_svm, max_iter = args.max_iter_svm)
+    elif(args.choix_modele == "fad"):
+        modele = FAD(nb_arbres = args.nb_arbres, critere = args.critere, prof_max = args.prof_max_fad)
+    elif(args.choix_modele == "adaboost"):
+        modele = AdaBoost(max_prof = args.prof_max_adaboost)
 
     # Entrainement ou validation croisée
-    if bool(vc) is False:
+    if bool(args.vc) is False:
         print("Début de l'entrainement simple...")
-        modele.entrainement(x_entr, t_entr, est_ech_poids, poids) # Il faudra fournir ech_poids de Gestion des données!!!
+        modele.entrainement(x_entr, t_entr, args.est_ech_poids, poids) # Il faudra fournir ech_poids de Gestion des données!!!
     else:
         print("Début de l'entrainement par validation croisée...")
-        modele.validation_croisee(x_entr, t_entr, 10, est_ech_poids, poids) # Il faudra fournir ech_poids de Gestion des données!!!
+        modele.validation_croisee(x_entr, t_entr, 10, args.est_ech_poids, poids) # Il faudra fournir ech_poids de Gestion des données!!!
 
     # Prédiction et erreur
     print("Calcul des erreurs...")
