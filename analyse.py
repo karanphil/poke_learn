1568d4c248bdad67161f562bb2848ed74d2c7ade
karanphil <philippe.karan@usherbrooke.ca>
2019-11-30 16:14:22 -0500

    écriture de la méthode calculer_comptes

--- analyse.py
+++ analyse.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import numpy as np
+from sklearn.metrics import confusion_matrix
 
 class Analyse:
     def __init__(self, verite_terrain, resultats, probabilites):
@@ -10,8 +12,14 @@ class Analyse:
         self.fp = 0
         self.fn = 0
 
-    def calculer_comptes(self):
-        raise NotImplementedError
+    def calculer_comptes(self, est_ech_poids = False, *args):
+        # Cas avec poids variables
+        if(est_ech_poids):
+            matrice_confusion = confusion_matrix(self.verite_terrain, self.resultats, sample_weight = args[0])
+        # Cas sans poids variables
+        else:
+            matrice_confusion = confusion_matrix(self.verite_terrain, self.resultats)
+        self.vn, self.fp, self.fn, self.vp = matrice_confusion.ravel()
     
     def calculer_rappel(self):
         raise NotImplementedError
